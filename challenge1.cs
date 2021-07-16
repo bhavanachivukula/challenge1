fe9d2ffddd3fa5b3225cf45d88bbf9f0561ef2aa
Bhavana Chivukula <bchivukula1999@gmail.com>
2021-07-16 21:15:09 +0530

    Addition feature is added

--- challenge1.cs
+++ challenge1.cs
@@ -5,8 +5,16 @@ namespace Challenge1
     {
         public static void Main(string[] args){
         Console.WriteLine("Hello world");
+        add();
         Console.ReadKey();
-    }   
+        }
+        static void add(){
+            int a=Convert.ToInt32(Console.ReadLine());
+            int b=Convert.ToInt32(Console.ReadLine());
+            int c=a+b;
+            Console.WriteLine("Addition program of a + b is"+c);
+        }
+   
     }
     
 }
